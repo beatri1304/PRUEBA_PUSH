c39571091e1725c617d9985de5d49271301940ec
beatri1304 <162139180+Beatri13@users.noreply.github.com>
2025-03-26 18:45:57 -0600

    Ni idea bb

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Persona
+{
+
+        // Propiedades
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        // Constructor
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+        // Método para mostrar información
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}");
+        }
+    
+
+    // Uso de la clase
+    class Program
+    {
+        static void Main()
+        {
+            Persona persona1 = new Persona("Juan", 30);
+            persona1.MostrarInformacion();
+        }
+    }
+
+}
+
